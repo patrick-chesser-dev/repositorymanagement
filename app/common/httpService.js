4c72cfaa9aae1606a5fbe05f33af55ca39e49d7c
Patrick Chesser <patrick.chesser@avalara.com>
2021-10-08 16:40:04 -0600

    test(httpService): cleaning up tests Figured out how to mock axios. Added Negative Test cases. Added Describes

--- app/common/httpService.js
+++ app/common/httpService.js
@@ -1,11 +1,18 @@
+const { NullArgumentError } = require('../common/errors');
 class HttpService {
     #axios = null;
 
     constructor(axios) {
+        if (!axios) {
+            throw new NullArgumentError('axios cannot be null');
+        }
         this.#axios = axios;
     }
 
     async unAuthenticatedGet(requestUrl) {
+        if (!requestUrl) {
+            throw new NullArgumentError('requestUrl cannot be null');
+        }
         try {
             return await this.#axios.get(requestUrl);
         } catch (ex) {
